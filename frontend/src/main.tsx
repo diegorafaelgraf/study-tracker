67855e48bbed84f373c308dadfd6db388c34c3cd
DIEGO RAFAEL GRAF <graf.drg@gmail.com>
2026-04-27 10:43:56 -0300

    add auth context and provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AuthProvider } from './context/auth.context.tsx';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </QueryClientProvider>
 );
