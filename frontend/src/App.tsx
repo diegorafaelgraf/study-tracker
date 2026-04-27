e0a043f6af3ff476e90de24bc70d8c978722d87a
DIEGO RAFAEL GRAF <graf.drg@gmail.com>
2026-04-27 10:37:45 -0300

    add routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,25 +5,44 @@ import YearTopics from './pages/Years/YearTopics';
 import Topics from './pages/Topics/Topics'
 import TopicYears from './pages/Topics/TopicYears';
 import NewPractice from './pages/Practices/NewPractice';
-import CreateYear from './pages/Admin/CreateYear';
+import CreateYear from './pages/Years/CreateYear';
+import Login from './pages/Auth/Login';
+import ChangePassword from './pages/Auth/ChangePassword';
+import ProtectedRoute from './components/ProtectedRoutes';
+import AdminRoute from './components/AdminRoutes';
+import CreateUser from './pages/Admin/CreateUser';
+// import Logout from './pages/Auth/Logout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
 
-        <Route path="/closed-years" element={<ClosedYears />} />
-        <Route path="/closed-years/:yearId" element={<YearTopics />} />
+        {/* LOGIN PAGE */}
+        <Route path="/login" element={<Login />} />
 
-        <Route path="/current-year/:yearId" element={<YearTopics />} />
+        {/* USERS PAGES */}
+        {/* Home Page */}
+        <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        {/* Closed Years Pages */}
+        <Route path="/closed-years" element={<ProtectedRoute><ClosedYears /></ProtectedRoute>} />
+        <Route path="/closed-years/:yearId" element={<ProtectedRoute><YearTopics /></ProtectedRoute>} />
+        {/* Current Year Page */}
+        <Route path="/current-year/:yearId" element={<ProtectedRoute><YearTopics /></ProtectedRoute>} />
+        {/* Topics Pages */}
+        <Route path="/topics" element={<ProtectedRoute><Topics /></ProtectedRoute>} />
+        <Route path="/topics/:topicId" element={<ProtectedRoute><TopicYears /></ProtectedRoute>} />
+        {/* Practices Pages */}
+        <Route path="/practices/new" element={<ProtectedRoute><NewPractice /></ProtectedRoute>} />
+        {/* Year Pages */}
+        <Route path="/years" element={<ProtectedRoute><CreateYear /></ProtectedRoute>} />
+        {/* Change Password Page */}
+        <Route path="/change-password" element={<ProtectedRoute><ChangePassword /></ProtectedRoute>} />
 
-        <Route path="/topics" element={<Topics />} />
-        <Route path="/topics/:topicId" element={<TopicYears />} />
+        {/* ADMIN PAGES */}
+        {/* User Pages */}
+        <Route path="/admin/users" element={<AdminRoute><CreateUser /></AdminRoute>} />
 
-        <Route path="/practices/new" element={<NewPractice />} />
-
-        <Route path="/admin/years" element={<CreateYear />} />
       </Routes>
     </BrowserRouter>
   );
