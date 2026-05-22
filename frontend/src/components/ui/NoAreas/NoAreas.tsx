15c6d338046e56cfcaa713bcdd627953eee5c3b4
DIEGO RAFAEL GRAF <graf.drg@gmail.com>
2026-05-22 13:53:00 -0300

    feat: :sparkles: Display the message “No areas” on the home page when the user has no areas assigned for the current year

---
+++ frontend/src/components/ui/NoAreas/NoAreas.tsx
@@ -0,0 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+import styles from './NoAreas.module.css';
+
+export default function NoAreas() {
+
+  return (
+    <div className={styles.container}>
+      <h1>No tienes ningún área de estudio asignada.</h1>
+      <p>
+        Para comenzar a registrar tus prácticas, primero debes asignar al menos un área de estudio al año vigente.
+      </p>
+      {/* 
+      //TODO: Implementar la funcionalidad para asignar un area de estudio al año vigente vía modal.
+      <button className={styles.submitBtn} onClick={() => navigate('/year-topics/assign')}>
+        Asignar área
+      </button> */}
+    </div>
+  );
+}
